de0d3b15023de062d30d5a2e5c9765068fdcffb2
Byron Jacquot <byron.jacquot@sparkfun.com>
2016-08-16 15:24:58 -0600

    Adding sample sketch example of bidirectional trigger.
    Got the pin direction question answered.
    
    There's an external pull down that tugs the line when the internal pulup
    gets defeated.

--- Firmware/hardware-tests/output-trigger/output-trigger.ino
+++ Firmware/hardware-tests/output-trigger/output-trigger.ino
@@ -15,19 +15,47 @@ static uint32_t next;
 static lp55231 ledChip(0x32);
 
 /*
- *
+ * More trickery at play here (not as tangled as the interrupt example).
+ * 
+ * The LP55231 trigger line can be used as both an input and an output.
+ * This example does exactly that...bit at first,v we couldn't explain why it worked.
+ * 
+ * The Trigger instruction on the LP allows for it listen for a trigger, or to drive 
+ * the trigger line.  If both options are used in the same instruction, it sends a 
+ * short (roughly 100 uSec) trigger pulse, then waits for a response.
+ * 
+ * This sketch waits until it sees that pulse, observes it going high again, then 
+ * drives the line.
+ * 
+ * But it wasn't entirely apparent why & how it was working.  The AVR port pin 
+ * architecture held the explanation.
+ * 
+ * The trigger pin on the pro micro is configured as INPUT_PULLUP in
+ * setup().  Loop polls that pin, looking for a low.  When it finds a low, it waits for 
+ * it to go high again, waits a touch longer, the drives it low itself.
+ * 
+ * Yes, it's an input...but it's allowed to drive it low, but somewhat indirectly.
+ * 
+ * When configured as an output, the data bits enable the pullup resistors.
+ * 
+ * So by writing a low to the output pin, we essentially defeat the pullup.
+ * The Trigger line is pulled low on the LP breakout, which takes over when the 
+ * PUP is defeated.
+ * 
+ * This will probably work with other AVR based microcontrollers, but 
+ * might not be portable beynd that.
  */
 
 static const uint16_t program[] =
 {
   0x9c09, // 0 map start addr
   0x9c8c, // 1 map end addr
-  0x3000, // 4 wait
+  0x3000, // 2 wait
   0x0eff, // 3 ramp up over 255
   0x3000, // 4 wait
   0x0fff, // 5 ramp down over 255
   0x9d80, // 6 mux map next
-  0xf040, // 7 send ext trigger
+  0xf040, // 7 trigger - send to micro, and wait for response.
   0xa002, // 8 loop to 2
   0x01ff, // 9 channel map - all
   0x01c0, // a channel map - reds
